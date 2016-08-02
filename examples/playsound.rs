979444adbedb09ce336b3767a89686d1da6c0835
Matthew D. Steele <mdsteele@alum.mit.edu>
2016-08-02 09:33:32 -0400

    Revamp API; add playsound example

---
+++ examples/playsound.rs
@@ -0,0 +1,117 @@
+extern crate itersynth;
+#[macro_use]
+extern crate nom;
+extern crate sdl2;
+
+use itersynth::{Wave, WaveGen};
+use std::str::{self, FromStr};
+use std::time::Duration;
+
+// ========================================================================= //
+
+named!(any_wave<Wave>,
+       alt!(const_wave | product_wave | pulse_wave | sine_wave | sum_wave));
+
+named!(const_wave<Wave>, map!(float_literal, Into::into));
+
+named!(product_wave<Wave>,
+       map!(preceded!(tag!("mul"),
+                      delimited!(char!('('),
+                                 separated_pair!(any_wave,
+                                                 char!(','),
+                                                 any_wave),
+                                 char!(')'))),
+            |(wave1, wave2)| wave1 * wave2));
+
+named!(pulse_wave<Wave>,
+       map!(preceded!(tag!("pulse"),
+                      delimited!(char!('('),
+                                 separated_pair!(any_wave,
+                                                 char!(','),
+                                                 any_wave),
+                                 char!(')'))),
+            |(freq, duty)| Wave::pulse(freq, duty)));
+
+named!(sine_wave<Wave>,
+       map!(preceded!(tag!("sine"),
+                      delimited!(char!('('),
+                                 any_wave,
+                                 char!(')'))),
+            Wave::sine));
+
+named!(sum_wave<Wave>,
+       map!(preceded!(tag!("add"),
+                      delimited!(char!('('),
+                                 separated_pair!(any_wave,
+                                                 char!(','),
+                                                 any_wave),
+                                 char!(')'))),
+            |(wave1, wave2)| wave1 + wave2));
+
+named!(float_literal<f32>,
+       map_res!(map_res!(recognize!(pair!(nom::digit,
+                                          opt!(pair!(char!('.'),
+                                                     nom::digit)))),
+                         str::from_utf8),
+                FromStr::from_str));
+
+// ========================================================================= //
+
+struct WaveCallback {
+    wave: itersynth::Wave,
+    audio_rate: i32,
+}
+
+impl WaveCallback {
+    fn new(wave: itersynth::Wave, audio_rate: i32) -> WaveCallback {
+        WaveCallback {
+            wave: wave,
+            audio_rate: audio_rate,
+        }
+    }
+}
+
+impl sdl2::audio::AudioCallback for WaveCallback {
+    type Channel = itersynth::Sample;
+
+    fn callback(&mut self, out: &mut [itersynth::Sample]) {
+        let audio_rate = self.audio_rate as f32;
+        for sample in out.iter_mut() {
+            *sample = self.wave.next(audio_rate).unwrap_or(0.0);
+        }
+    }
+}
+
+// ========================================================================= //
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let spec: &[u8] = if args.len() >= 2 {
+        args[1].as_bytes()
+    } else {
+        b"sine(440)"
+    };
+    let wave = match any_wave(spec) {
+        nom::IResult::Done(_, wave) => wave,
+        _ => {
+            println!("Failed to parse spec.");
+            return;
+        }
+    };
+
+    let sdl_context = sdl2::init().unwrap();
+    let audio_subsystem = sdl_context.audio().unwrap();
+    let desired_spec = sdl2::audio::AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1), // mono
+        samples: None, // default sample size
+    };
+    let device = audio_subsystem.open_playback(None, &desired_spec, |spec| {
+                                    WaveCallback::new(wave, spec.freq)
+                                })
+                                .unwrap();
+    device.resume();
+    std::thread::sleep(Duration::from_millis(2000));
+}
+
+// ========================================================================= //
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![warn(missing_docs)]
 
 use std::f32::consts::PI;
+use std::ops::{Add, Mul};
 
 /// One sample value from a waveform.
 ///
@@ -10,8 +11,18 @@ use std::f32::consts::PI;
 /// constant value forever.
 pub type Sample = f32;
 
-impl Wave for Sample {
-    fn next(&mut self) -> Option<Sample> {
+/// A waveform generator.
+pub trait WaveGen: Send {
+    /// Gets the next sample value, or returns `None` if the waveform has
+    /// finished.  The `sample_rate` gives the number of samples per second.
+    fn next(&mut self, sample_rate: f32) -> Option<Sample>;
+
+    /// Resets the waveform back to the beginning.
+    fn reset(&mut self);
+}
+
+impl WaveGen for Sample {
+    fn next(&mut self, _: f32) -> Option<Sample> {
         Some(*self)
     }
 
@@ -22,40 +33,86 @@ impl Wave for Sample {
 ///
 /// Normally the samples will represent sound amplitude values, but they can
 /// also represent frequencies or other values, depending on context.
-pub trait Wave: Clone + Send {
-    /// Gets the next sample value, or returns `None` if the waveform has
-    /// finished.
-    fn next(&mut self) -> Option<Sample>;
+pub struct Wave {
+    generator: Box<WaveGen>,
+}
 
-    /// Resets the waveform back to the beginning.
-    fn reset(&mut self);
-
-    /// Returns a new waveform that repeats this one forever.
-    fn looped(self) -> Looped<Self> {
-        Looped { wave: self }
+impl Wave {
+    /// Creates a sine wave, with an amplitude of 1, whose frequency over time
+    /// is controlled by the input waveform (which may be a constant).  The
+    /// input frequency values are measured in hertz (cycles per second).
+    pub fn sine<F: Into<Wave>>(freq: F) -> Wave {
+        Wave::new(Box::new(SineWave::new(freq.into())))
     }
 
-    /// Sums two waveforms together; the new waveform ends only when both input
-    /// waveforms have ended.
-    fn add<W: Wave>(self, other: W) -> Sum<Self, W> {
-        Sum {
+    /// Creates a pulse wave whose frequency over time is controlled by the
+    /// `freq` waveform, and whose duty cycle over time is controlled by the
+    /// `duty` waveform (either or both of which may be constants).  The input
+    /// frequency values are measured in hertz (cycles per second); the input
+    /// duty values should be between 0 and 1 (with 0.5 being a square wave).
+    pub fn pulse<F: Into<Wave>, D: Into<Wave>>(freq: F, duty: D) -> Wave {
+        Wave::new(Box::new(PulseWave::new(freq.into(), duty.into())))
+    }
+
+    /// Creates a waveform using the given generator.
+    pub fn new(generator: Box<WaveGen>) -> Wave {
+        Wave { generator: generator }
+    }
+
+    /// Returns a new waveform that repeats this one forever.
+    pub fn looped(self) -> Wave {
+        Wave::new(Box::new(Looped { wave: self }))
+    }
+}
+
+impl<W: Into<Wave>> Add<W> for Wave {
+    type Output = Wave;
+
+    fn add(self, rhs: W) -> Wave {
+        Wave::new(Box::new(Sum {
             wave1: self,
-            wave2: other,
-        }
+            wave2: rhs.into(),
+        }))
+    }
+}
+
+impl From<Sample> for Wave {
+    fn from(sample: Sample) -> Wave {
+        Wave::new(Box::new(sample))
+    }
+}
+
+impl<W: Into<Wave>> Mul<W> for Wave {
+    type Output = Wave;
+
+    fn mul(self, rhs: W) -> Wave {
+        Wave::new(Box::new(Product {
+            wave1: self,
+            wave2: rhs.into(),
+        }))
+    }
+}
+
+impl WaveGen for Wave {
+    fn next(&mut self, sample_rate: f32) -> Option<Sample> {
+        self.generator.next(sample_rate)
+    }
+
+    fn reset(&mut self) {
+        self.generator.reset();
     }
 }
 
 /// A waveform consisting of some other waveform, repeated indefinitely.
-#[derive(Clone)]
-pub struct Looped<W> {
-    wave: W,
+struct Looped {
+    wave: Wave,
 }
 
-impl<W: Wave> Wave for Looped<W> {
-    fn next(&mut self) -> Option<Sample> {
-        self.wave.next().or_else(|| {
+impl WaveGen for Looped {
+    fn next(&mut self, sample_rate: f32) -> Option<Sample> {
+        self.wave.next(sample_rate).or_else(|| {
             self.wave.reset();
-            self.wave.next()
+            self.wave.next(sample_rate)
         })
     }
 
@@ -64,19 +121,82 @@ impl<W: Wave> Wave for Looped<W> {
     }
 }
 
-/// A variable-frequency sine wave, with an amplitude of 1.
-#[derive(Clone)]
-pub struct SineWave<F> {
-    freq: F,
+/// A waveform consisting of the product of two other waveforms.
+struct Product {
+    wave1: Wave,
+    wave2: Wave,
+}
+
+impl WaveGen for Product {
+    fn next(&mut self, sample_rate: f32) -> Option<Sample> {
+        match self.wave1.next(sample_rate) {
+            Some(value1) => {
+                match self.wave2.next(sample_rate) {
+                    Some(value2) => Some(value1 * value2),
+                    None => None,
+                }
+            }
+            None => None,
+        }
+    }
+
+    fn reset(&mut self) {
+        self.wave1.reset();
+        self.wave2.reset();
+    }
+}
+
+/// A variable-frequency, variable-duty pulse wave, with an amplitude of 1.
+struct PulseWave {
+    freq: Wave,
+    duty: Wave,
     phase: f32,
 }
 
-impl<F> SineWave<F> {
-    /// Creates a sine wave whose frequency over time is controlled by the
-    /// input waveform (which may be a constant).  The input frequency values
-    /// are measured in cycles per sample; to convert from hertz (cycles per
-    /// second), divide by the audio rate (samples per second) being used.
-    pub fn new(freq: F) -> SineWave<F> {
+impl PulseWave {
+    fn new(freq: Wave, duty: Wave) -> PulseWave {
+        PulseWave {
+            freq: freq,
+            duty: duty,
+            phase: 0.0,
+        }
+    }
+}
+
+impl WaveGen for PulseWave {
+    fn next(&mut self, audio_rate: f32) -> Option<Sample> {
+        let freq = match self.freq.next(audio_rate) {
+            Some(freq) => freq,
+            None => return None,
+        };
+        let duty = match self.duty.next(audio_rate) {
+            Some(duty) => duty,
+            None => return None,
+        };
+        let phase = self.phase;
+        self.phase = (self.phase + freq / audio_rate) % 1.0;
+        Some(if phase < duty {
+            1.0
+        } else {
+            -1.0
+        })
+    }
+
+    fn reset(&mut self) {
+        self.freq.reset();
+        self.duty.reset();
+        self.phase = 0.0;
+    }
+}
+
+/// A variable-frequency sine wave, with an amplitude of 1.
+struct SineWave {
+    freq: Wave,
+    phase: f32,
+}
+
+impl SineWave {
+    fn new(freq: Wave) -> SineWave {
         SineWave {
             freq: freq,
             phase: 0.0,
@@ -84,14 +204,14 @@ impl<F> SineWave<F> {
     }
 }
 
-impl<F: Wave> Wave for SineWave<F> {
-    fn next(&mut self) -> Option<Sample> {
-        let freq = match self.freq.next() {
+impl WaveGen for SineWave {
+    fn next(&mut self, sample_rate: f32) -> Option<Sample> {
+        let freq = match self.freq.next(sample_rate) {
             Some(freq) => freq,
             None => return None,
         };
         let phase = self.phase;
-        self.phase = (self.phase + freq) % 1.0;
+        self.phase = (self.phase + freq / sample_rate) % 1.0;
         Some((2.0 * PI * phase).sin())
     }
 
@@ -102,22 +222,21 @@ impl<F: Wave> Wave for SineWave<F> {
 }
 
 /// A waveform consisting of the sum of two other waveforms.
-#[derive(Clone)]
-pub struct Sum<W, V> {
-    wave1: W,
-    wave2: V,
+struct Sum {
+    wave1: Wave,
+    wave2: Wave,
 }
 
-impl<W: Wave, V: Wave> Wave for Sum<W, V> {
-    fn next(&mut self) -> Option<Sample> {
-        match self.wave1.next() {
+impl WaveGen for Sum {
+    fn next(&mut self, sample_rate: f32) -> Option<Sample> {
+        match self.wave1.next(sample_rate) {
             Some(value1) => {
-                Some(match self.wave2.next() {
+                Some(match self.wave2.next(sample_rate) {
                     Some(value2) => value1 + value2,
                     None => value1,
                 })
             }
-            None => self.wave2.next(),
+            None => self.wave2.next(sample_rate),
         }
     }
 
@@ -148,28 +267,29 @@ mod tests {
 
     #[test]
     fn sine_wave() {
-        let mut wave = SineWave::new(0.125);
-        assert_approx!(0.0, wave.next().unwrap());
-        assert_approx!(0.5 * SQRT_2, wave.next().unwrap());
-        assert_approx!(1.0, wave.next().unwrap());
-        assert_approx!(0.5 * SQRT_2, wave.next().unwrap());
-        assert_approx!(0.0, wave.next().unwrap());
-        assert_approx!(-0.5 * SQRT_2, wave.next().unwrap());
-        assert_approx!(-1.0, wave.next().unwrap());
-        assert_approx!(-0.5 * SQRT_2, wave.next().unwrap());
-        assert_approx!(0.0, wave.next().unwrap());
+        let sample_rate = 22050.0;
+        let mut wave = Wave::sine(2756.25);
+        assert_approx!(0.0, wave.next(sample_rate).unwrap());
+        assert_approx!(0.5 * SQRT_2, wave.next(sample_rate).unwrap());
+        assert_approx!(1.0, wave.next(sample_rate).unwrap());
+        assert_approx!(0.5 * SQRT_2, wave.next(sample_rate).unwrap());
+        assert_approx!(0.0, wave.next(sample_rate).unwrap());
+        assert_approx!(-0.5 * SQRT_2, wave.next(sample_rate).unwrap());
+        assert_approx!(-1.0, wave.next(sample_rate).unwrap());
+        assert_approx!(-0.5 * SQRT_2, wave.next(sample_rate).unwrap());
+        assert_approx!(0.0, wave.next(sample_rate).unwrap());
         wave.reset();
-        assert_approx!(0.0, wave.next().unwrap());
-        assert_approx!(0.5 * SQRT_2, wave.next().unwrap());
-        assert_approx!(1.0, wave.next().unwrap());
+        assert_approx!(0.0, wave.next(sample_rate).unwrap());
+        assert_approx!(0.5 * SQRT_2, wave.next(sample_rate).unwrap());
+        assert_approx!(1.0, wave.next(sample_rate).unwrap());
     }
 
     #[test]
     fn wave_sum() {
-        let mut wave = SineWave::new(0.25).add(1.5);
-        assert_approx!(1.5, wave.next().unwrap());
-        assert_approx!(2.5, wave.next().unwrap());
-        assert_approx!(1.5, wave.next().unwrap());
-        assert_approx!(0.5, wave.next().unwrap());
+        let mut wave = Wave::sine(0.25) + 1.5;
+        assert_approx!(1.5, wave.next(1.0).unwrap());
+        assert_approx!(2.5, wave.next(1.0).unwrap());
+        assert_approx!(1.5, wave.next(1.0).unwrap());
+        assert_approx!(0.5, wave.next(1.0).unwrap());
     }
 }
